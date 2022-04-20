2e7fa54c76d3a5708bddd86b82d052ef926c33fe
Himanshu Srivastava <h.dataconsultancy@gmail.com>
2022-04-20 09:48:31 +0530

    Initial project setup using create-react-app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const element = <h1>Hello, World</h1>;
+	// This syntax is neither js not HTML. This is jsx.
+
+	// React embraces the fact that rendering logic is inherently coupled
+	// with other UI logic:
+	// Instead of artificially separating technologies by putting markup and
+	// logic in separate files, React separates concerns with loosely coupled
+	// units called “components” that contain both.
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> {element}
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
